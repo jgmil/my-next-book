b0f19569f770219476fc1e1ed885537aae85ec30
Julia Miller <opie.julia@gmail.com>
2018-03-07 21:36:45 -0500

    initial file structure

---
+++ index.js
@@ -0,0 +1,31 @@
+const TASTEDIVE_SEARCH_URL = 'https://tastedive.com/api/similar?';
+
+function getData(searchTerm, callback) {
+  const query = {
+    k: '302151-MyNextBo-ICUKSR70',
+    q: `the help`,
+    type: books
+  };
+  $.getJSON(TASTEDIVE_SEARCH_URL, query, callback);
+}
+
+function onSubmit() {
+    $('.searchForm').submit(event => {
+    event.preventDefault();
+    const query = {
+        k: '302151-MyNextBo-ICUKSR70',
+        q: `the help`,
+        type: 'books'
+    };
+    $.getJSON(TASTEDIVE_SEARCH_URL, query, callback);
+    getData(query, displayData);
+  });
+}
+
+
+function displayData(data) {
+  console.log(data);
+  }
+
+
+$(onSubmit);
